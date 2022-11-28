6e807bd7b93de46663a25978ab3ca4880a496e5b
Chrispolanco <chrispolanco@sbcglobal.net>
2022-11-27 17:36:06 -0800

    car updated for array

--- section-seven/Car.java
+++ section-seven/Car.java
@@ -1,13 +1,70 @@
-public class Car {
-    String make;
-    double price;
-    int year;
-    String color;
+import java.util.Arrays;
 
-    public Car(String make, double price, int year, String color){
+public class Car {
+    private String make;
+    private double price;
+    private int year;
+    private String color;
+    private String[] parts;
+
+    public Car(String make, double price, int year, String color, String[] parts){
         this.make = make;
         this.price = price;
         this.year = year;
         this.color = color;
+        this.parts = Arrays.copyOf(parts, parts.length);
+    }
+
+    public Car(Car source){
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = source.color;
+        this.parts = Arrays.copyOf(source.parts, source.parts.length);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String[] getParts() {
+        return Arrays.copyOf(parts, parts.length);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setParts(String[] parts) {
+        this.parts = parts;
+    }
+
+    public void drive(){
+        System.out.println("\nYou bought the beautiful " + year + " " + color + " " + make + " for " + price + ".");
+        System.out.println("Please drive your car to the nearest exist.");
     }
 }
--- section-seven/Main.java
+++ section-seven/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args){
-        Car nissan = new Car("Nissan", 1000, 2020, "Green");
 
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+        Car nissan = new Car("Nissan", 10000, 2020, "Green", new String[] {"Tires", "Keys"});
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue", new String[] {"Tires", "Keys"});
+//        String[] carParts = nissan.getParts();
+//        carParts[0] = "Filter";
+//        System.out.println(carParts[0]);
+//        System.out.println(dodge.getParts()[0]);
+//        System.out.println(nissan.getParts()[0]);
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price +
-                ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price +
-                ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
+        Car nissan2 = new Car(nissan);
+        nissan2.setColor("Teal");
+//        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
+//                ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+//        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
+//                ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
+        nissan2.setColor("Yellow");
+        nissan2.drive();
     }
 }
